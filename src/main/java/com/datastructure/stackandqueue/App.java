170fe998c956c8a77fb242385a78443255ccedbd
Kunal <kunalsonawane730@gmail.com>
2022-11-27 11:56:55 +0530

    adding welcome message for stack and queue programs

---
+++ src/main/java/com/datastructure/stackandqueue/App.java
@@ -0,0 +1,9 @@
+package com.datastructure.stackandqueue;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome To Stack and Queue Data Structure" );
+    }
+}
